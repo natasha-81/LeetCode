7d9d8f8486ae5721d13c49159e0400a1be47e91c
Natasha <8181natasha@gmail.com>
2026-07-13 23:25:05 +0530

    Time: 0 ms (100%), Space: 8.3 MB (76.91%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        string s = "123456789";
+        vector<int> result;
+        for (int length=2; length<=9; length++) {
+            for (int i=0; i+length<=9; i++) {
+                int num = stoi(s.substr(i,length));
+                if (num>=low && num<=high) {
+                    result.push_back(num);
+                }
+            }
+        }
+        return result;
+    }
+};
