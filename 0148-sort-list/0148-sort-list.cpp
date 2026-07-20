14912bafc8a9a5170c82cbf860a9a30f88f04b4c
Natasha <8181natasha@gmail.com>
2026-07-21 01:17:53 +0530

    Time: 7 ms (95.45%), Space: 58 MB (70.77%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,53 +10,20 @@
  */
 class Solution {
 public:
-    ListNode* mergetwolists(ListNode* leftNode, ListNode* rightNode) {
-        if (leftNode == NULL && rightNode == NULL ) return NULL;
-        if (leftNode == NULL && rightNode != NULL) return rightNode;
-        if (rightNode == NULL && leftNode != NULL ) return leftNode;
-        ListNode* dummyNode = new ListNode(-1);
-        ListNode* lefthead = leftNode;
-        ListNode* righthead = rightNode;
-        ListNode* temp = dummyNode;
-        while (lefthead != NULL && righthead != NULL) {
-            if (lefthead->val <= righthead->val) {
-                temp->next = lefthead;
-                temp = lefthead;
-                lefthead = lefthead->next;
-            }
-            else {
-                temp->next = righthead;
-                temp = righthead;
-                righthead = righthead->next;
-            }
-        }
-        if (lefthead != NULL) {
-            temp->next = lefthead;
-        }
-        else {
-            temp->next = righthead;
-        }
-        return dummyNode->next;
-    }
-    ListNode* findMiddle(ListNode* head) {
-        if (head == NULL || head-> next == NULL ) return head;
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow -> next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
     ListNode* sortList(ListNode* head) {
-        if (head == NULL || head->next == NULL) return head;
-        ListNode* middle = findMiddle(head);
-        ListNode* rightNode = middle->next;
-        middle->next=NULL;
-        ListNode* leftNode = head;
-        leftNode = sortList(leftNode);
-        rightNode = sortList(rightNode);
-        return mergetwolists(leftNode,rightNode);
+         vector<int> arr;
+        ListNode* temp = head;
+        while(temp!=nullptr){
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
+        sort(arr.begin(),arr.end());
+        temp=head;
+        for(int i=0;i<arr.size();i++){
+            temp->val = arr[i];
+            temp=temp->next;
+        }
+        return head;
 
     }
 };
