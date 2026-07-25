a9fb7b09f7124653c8d9b1a2aaf13d66f70eca4f
Natasha <8181natasha@gmail.com>
2026-07-25 20:44:11 +0530

    Time: 0 ms (100%), Space: 7.8 MB (77.41%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n<=0) return false;
+        if (n==1) return true;
+        // to check if number is divided by 2,3,5
+        if (n%2==0) {
+            return isUgly(n/2);
+        }
+        if (n%3==0) {
+            return isUgly(n/3);
+        }
+        if (n%5==0) {
+            return isUgly(n/5);
+        }
+        return false;
+    }
+};
