c2f5a4ef424e751d7c2ff4b2658311221abdbf42
Natasha <8181natasha@gmail.com>
2026-07-27 00:11:21 +0530

    Time: 0 ms (100%), Space: 10 MB (9.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for (auto it : s) {
+            if (it == '(' || it == '{' || it == '[') {
+                st.push(it);
+            }
+            else {
+                if (st.empty()) return false; // no matching opening bracket
+                char ch = st.top();
+                st.pop();
+                if ((it == ')' && ch == '(') ||
+                    (it == '}' && ch == '{') ||
+                    (it == ']' && ch == '['))
+                    continue;
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
